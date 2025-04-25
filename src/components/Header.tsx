f41fed8d56eb126f372569f7f61062658cf2f26b
Kenan Smajic <ksmajic996@gmail.com>
2025-04-25 23:39:25 +0200

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -74,7 +74,7 @@ export function Header({ scrollToPricing, scrollToServices, scrollToContact, scr
           : 'bg-white/30 backdrop-blur-sm'
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <Link to="/" className="flex items-center space-x-2">
             
